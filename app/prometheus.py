a331be15921bf9dbcb01b32cddb69c0e594ecced
Serhii Kolomiiets <iptoname@gmail.com>
2019-01-10 22:56:52 +0200

    init

---
+++ app/prometheus.py
@@ -0,0 +1,40 @@
+import random, time
+
+from flask import Flask, render_template_string, abort
+from prometheus_client import generate_latest, REGISTRY, Counter, Gauge, Histogram
+
+app = Flask(__name__)
+
+REQUESTS = Counter('http_requests_total', 
+                   'Total HTTP Requests (count)', 
+                   ['method', 'endpoint', 'status_code'])
+
+IN_PROGRESS = Gauge('http_requests_inprogress', 
+                    'Number of in progress HTTP requests')
+TIMINGS = Histogram('http_request_duration_seconds', 'HTTP request latency (seconds)')
+
+@app.route('/')
+@TIMINGS.time()
+@IN_PROGRESS.track_inprogress()
+def hello_world():
+    REQUESTS.labels(method='GET', endpoint="/", status_code=200).inc() # Increment the counter
+    return 'Hello, World!'
+
+@app.route('/prometheus-course/<name>')
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
+def index(name):
+    REQUESTS.labels(method='GET', 
+                    endpoint="prometheus-course/<name>", 
+                    status_code=200).inc()
+    return render_template_string('<b>Hello {{name}} welcome!</b>', name=name)
+
+@app.route('/metrics')
+@IN_PROGRESS.track_inprogress()
+@TIMINGS.time()
+def metrics():
+    REQUESTS.labels(method='GET', endpoint="/metrics", status_code=200).inc()
+    return generate_latest(REGISTRY)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='80')
